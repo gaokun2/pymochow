e2f738d6fb1e90b1c8503367d59fa430f18cea75
guobo <guobo@baidu.com>
2023-11-17 11:01:15 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,34 @@
+Mochow Python Sdk
+===
+莫愁向量数据库的PythonSDK。
+
+快速开始
+---
+如何构建、安装、运行
+
+测试
+---
+如何执行自动化测试
+
+如何贡献
+---
+贡献patch流程及质量要求
+
+版本信息
+---
+本项目的各版本信息和变更历史可以在[这里][changelog]查看。
+
+维护者
+---
+### owners
+* guobo(guobo@baidu.com)
+
+### committers
+* guobo(guobo@baidu.com)
+
+讨论
+---
+百度Hi交流群：群号
+
+
+[changelog]: http://icode.baidu.com/repos/baidu/vdb/mochow-python-sdk/blob/master:CHANGELOG.md
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+################################################################################
+#
+# Copyright (c) 2023 Baidu.com, Inc. All Rights Reserved
+#
+################################################################################
+"""
+Setup script.
+
+Authors: guobo(guobo@baidu.com)
+Date:    2023/11/17 11:01:15
+"""
+
+import setuptools
+
+setuptools.setup()
+
