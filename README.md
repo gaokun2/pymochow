44bee762dc685e8be4ee4eb1ab27c2a729a8bc6c
fengjialin <fengjialin@baidu.com>
2024-01-19 14:36:42 +0800

    mochow-39 update sdk
    Change-Id: I7f49679f23d15354f78675abaf51d9195d832e2b

--- README.md
+++ README.md
@@ -1,34 +1,52 @@
-Mochow Python Sdk
-===
-莫愁向量数据库的PythonSDK。
+# 百度向量数据库 Mochow Python SDK
 
-快速开始
----
-如何构建、安装、运行
+针对百度智能云向量数据库，我们推出了一套 Python SDK（下称Mochow SDK），方便用户通过代码调用百度向量数据库。
 
-测试
----
-如何执行自动化测试
+## 如何安装
 
-如何贡献
----
-贡献patch流程及质量要求
+目前Mochow SDK 已发布到 PyPI ，用户可使用 pip 命令进行安装。安装Mochow SDK 需要 3.7.0 或更高的 Python 版本
 
-版本信息
----
-本项目的各版本信息和变更历史可以在[这里][changelog]查看。
+```
+pip install pymochow
+```
 
-维护者
----
-### owners
-* guobo(guobo@baidu.com)
+在安装完成后，用户即可在代码内引入Mochow SDK 并使用
 
-### committers
-* guobo(guobo@baidu.com)
+```python
+import pymochow
+```
 
-讨论
----
-百度Hi交流群：群号
+## 快速使用
 
+在使用Mochow SDK 之前，用户需要在百度云上创建向量数据库，以获得 API Key。API Key 是用户在调用Mochow SDK 时所需要的凭证。具体获取流程参见平台的[向量数据库使用说明文档](https://cloud.baidu.com/)。
 
-[changelog]: http://icode.baidu.com/repos/baidu/vdb/mochow-python-sdk/blob/master:CHANGELOG.md
+获取到 API Key 后，用户还需要传递它们来初始化Mochow SDK。 可以通过如下方式初始化Mochow SDK：
+
+```python
+import pymochow
+from pymochow.configuration import Configuration
+from pymochow.auth.bce_credentials import BceCredentials
+
+account = 'root'
+api_key = 'your_api_key'
+endpoint = 'you_endpoint' #example: http://127.0.0.1:8511
+
+config = Configuration(credentials=BceCredentials(account, api_key),
+            endpoint=endpoint)
+client = pymochow.MochowClient(config)
+```
+
+## 功能
+
+目前Mochow SDK 支持用户使用如下功能:
+
++ Databse 操作
++ Table 操作
++ Aliase 操作
++ Index 操作
++ Row 操作
+
+## License
+
+Apache-2.0
+
--- setup.py
+++ setup.py
@@ -11,35 +11,34 @@ Setup script.
 Authors: guobo(guobo@baidu.com)
 Date:    2023/11/17 11:01:15
 """
-from __future__ import absolute_import
 import io
 import os
 import re
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
-
-with io.open(os.path.join("pymochow", "__init__.py"), "rt") as f:
+with io.open(os.path.join("pymochow", "__init__.py"), "rt", encoding='utf-8') as f:
     SDK_VERSION = re.search(r"SDK_VERSION = b'(.*?)'", f.read()).group(1)
 
 setup(
     name='pymochow',
     version=SDK_VERSION,
-    install_requires=['requests',
-                      'orjson'],
+    install_requires=[
+        'requests',
+        'orjson',
+        'future'
+    ],
     python_requires='>=3.7',
-    packages=['pymochow',
-              'pymochow.auth',
-              'pymochow.http',
-              'pymochow.retry',
-              'pymochow.client',
-              'pymochow.model'
-              ],
+    packages=[
+        'pymochow',
+        'pymochow.auth',
+        'pymochow.http',
+        'pymochow.retry',
+        'pymochow.client',
+        'pymochow.model'
+    ],
     url='http://bce.baidu.com',
     license='Apache License 2.0',
-    author='',
-    author_email='',
-    description='Mochow SDK for python'
+    author='fengjialin',
+    author_email='fengjialin@baidu.com',
+    description='Python SDK for mochow'
 )
