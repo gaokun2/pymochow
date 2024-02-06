2749b0659e7ed5ef4d75500cf917f6affcaac00d
fengjialin <fengjialin@baidu.com>
2024-02-06 13:43:09 +0800

    mochow-39 update license
    Change-Id: I4a6966ae195bd23d7a5218fd4bb2b43d2f0ff03e

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Release Note
 
-## 0.0.2
+## 1.0.1
 
 Feature:
 - 支持mochow的API接口
--- pymochow/protocol.py
+++ pymochow/protocol.py
@@ -1,4 +1,4 @@
-# Copyright 2014 Baidu, Inc.
+# Copyright 2023 Baidu, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
 # except in compliance with the License. You may obtain a copy of the License at
@@ -9,6 +9,7 @@
 # License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 # either express or implied. See the License for the specific language governing permissions
 # and limitations under the License.
+
 """
 This module defines constants that represent the communication protocol to use when sending requests to BCE.
 
